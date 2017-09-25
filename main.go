11b75b3fbd84350e3c8c809bd62c12addd5e9acd
edfungus <edfungus@gmail.com>
2017-09-24 21:41:41 -0700

    Proof of concept with just two kafka topics

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,6 +14,10 @@ import (
 )
 
 var log = logging.MustGetLogger("eevee")
+
+type opaque struct {
+	Value string
+}
 
 func main() {
 	startLogger()
@@ -28,10 +34,11 @@ func main() {
 	}
 	defer c.Close()
 
-	topic := "test-topic"
-	err = c.Subscribe(topic, nil)
+	topicKAFKA := "kafka-in-out"
+	topicMQTT := "mqtt-in-out"
+	err = c.SubscribeTopics([]string{topicKAFKA, topicMQTT}, nil)
 	if err != nil {
-		log.Fatalf("Could not subsribe to topics: %s", topic)
+		log.Fatal("Could not subsribe to topics:")
 	}
 
 	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
@@ -52,25 +59,30 @@ func main() {
 	ticker := time.NewTicker(time.Second * 5)
 	go func() {
 		log.Noticef("Producer is starting up")
-
+		i := 0
 	runP:
 		for {
 			select {
 			case <-ticker.C:
-				log.Infof("Sending a message...")
-				p.ProduceChannel() <- &kafka.Message{
-					TopicPartition: kafka.TopicPartition{
-						Topic:     &topic,
-						Partition: kafka.PartitionAny,
-					},
-					Value: []byte("hello!"),
-				}
+				// log.Infof("Sending a message...")
+				// p.ProduceChannel() <- &kafka.Message{
+				// 	TopicPartition: kafka.TopicPartition{
+				// 		Topic:     &topic,
+				// 		Partition: kafka.PartitionAny,
+				// 	},
+				// 	Value: []byte("hello!"),
+				// 	Key:   []byte(fmt.Sprintf("Op%d", i)),
+				// }
 			case <-ctxP.Done():
 				log.Noticef("Producer is shutting down")
 				break runP
 			}
+			i++
 		}
 	}()
+
+	processedFromMQTTKeys := make(map[int]bool)
+	processedFromKafkaKeys := make(map[int]bool)
 
 	go func() {
 		log.Noticef("Consumer is starting up")
@@ -84,11 +96,42 @@ func main() {
 			case event := <-c.Events():
 				switch e := event.(type) {
 				case *kafka.Message:
-					op, ok := e.Opaque.([]byte)
-					if !ok {
-						op = []byte("")
+					if e.TopicPartition.Topic == nil {
+						log.Criticalf("Topic was nil?")
+						continue
 					}
-					log.Noticef("Got a message: %s | key: %s | opaque: %s", string(e.Value), string(e.Key), string(op))
+					// continue
+					topic := *e.TopicPartition.Topic
+					switch topic {
+					case topicMQTT:
+						log.Critical("IN MQTT TOPIC")
+						processed := checkKeyInHashset(processedFromKafkaKeys, e.Key)
+						if !processed {
+							key, number := generateKey()
+							log.Info("NUMBER GEN", "number", number)
+							log.Info("KEY OUT", "key", key)
+							addKeyToHashset(processedFromMQTTKeys, number)
+							log.Criticalf("CHECK HASHSET", "here?", processedFromMQTTKeys[number])
+							forwardToKafkaInOut(p, topicKAFKA, e.Value, key)
+						} else {
+							log.Noticef("MQTT | value: %s | key: %s ", string(e.Value), string(e.Key))
+							removeKeyFromHashset(processedFromKafkaKeys, e.Key)
+						}
+					case topic:
+						log.Critical("IN KAFKA TOPIC")
+						processed := checkKeyInHashset(processedFromMQTTKeys, e.Key)
+						if !processed {
+							number := rand.Int()
+							key, number := generateKey()
+							log.Info("NUMBER GEN", "number", number)
+							log.Info("KEY OUT", "key", key)
+							addKeyToHashset(processedFromKafkaKeys, number)
+							forwardToKafkaInOut(p, topicMQTT, e.Value, key)
+						} else {
+							log.Noticef("Kafka | value: %s | key: %s ", string(e.Value), string(e.Key))
+							removeKeyFromHashset(processedFromMQTTKeys, e.Key)
+						}
+					}
 				case kafka.AssignedPartitions:
 					log.Infof("%% %v\n", e)
 					c.Assign(e.Partitions)
@@ -106,13 +149,10 @@ func main() {
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		<-sigchan
-		cancelCtx()
-		time.Sleep(time.Second * 1)
-		break
-	}
 
+	<-sigchan
+	cancelCtx()
+	time.Sleep(time.Second * 1)
 	log.Noticef("Eevee says goodbye")
 
 }
@@ -122,4 +162,45 @@ func startLogger() {
 	format := logging.MustStringFormatter(`%{color}%{shortfunc} ▶ %{level:.4s} %{color:reset} %{message}`)
 	backendFormatter := logging.NewBackendFormatter(backend, format)
 	logging.SetBackend(backendFormatter)
+}
+
+func generateKey() (token []byte, number int) {
+	token = make([]byte, 4)
+	rand.Read(token)
+	number = int(binary.LittleEndian.Uint16(token))
+	return token, number
+}
+
+func addKeyToHashset(hashset map[int]bool, key int) {
+	hashset[key] = true
+}
+
+func forwardToKafkaInOut(p *kafka.Producer, topic string, value []byte, key []byte) {
+	log.Critical("SENDING MESSAGE TO", "topic", topic)
+	p.ProduceChannel() <- &kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: value,
+		Key:   key,
+	}
+}
+
+func checkKeyInHashset(hashset map[int]bool, key []byte) bool {
+	log.Info("KEY IN", "key", key)
+	log.Info("KEY LENGTH", "key", len(key))
+	if len(key) == 0 {
+		return false
+	}
+	number := int(binary.LittleEndian.Uint16(key))
+	log.Critical("NUMBER FIND", "number", number)
+	log.Criticalf("HASHSET", "set", hashset)
+	_, ok := hashset[number]
+	return ok
+}
+
+func removeKeyFromHashset(hashset map[int]bool, key []byte) {
+	number := int(binary.LittleEndian.Uint16(key))
+	delete(hashset, number)
 }
